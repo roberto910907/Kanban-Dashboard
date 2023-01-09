5903f7b93f315e12101b0c64ab4ca946544cf984
Roberto Rielo <roberto910907@gmail.com>
2023-01-09 06:58:21 -0500

    Added spinner component

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -2,8 +2,13 @@
   <div class="navbar">
     <div class="flex">
       <p class="grow">Kanban Board</p>
-      <button class="button primary" @click="exportDatabase">
-        Export Database
+      <button
+        class="button primary"
+        :disabled="creating"
+        @click="exportDatabase"
+      >
+        <Spinner v-if="creating" class="mr-1"></Spinner>
+        {{ exportButtonText }}
       </button>
     </div>
   </div>
@@ -11,11 +16,27 @@
 
 <script>
 import axios from 'axios';
+import Spinner from './Spinner.vue';
 
 export default {
   name: 'Navbar',
+  components: { Spinner },
+  data() {
+    return {
+      creating: false,
+    };
+  },
+
+  computed: {
+    exportButtonText() {
+      return this.creating ? 'Exporting...' : 'Export Database';
+    },
+  },
+
   methods: {
     async exportDatabase() {
+      this.creating = true;
+
       try {
         const { data } = await axios.get('/api/export/database');
 
@@ -25,6 +46,8 @@ export default {
       } catch (error) {
         // Show UI error notification
       }
+
+      this.creating = false;
     },
   },
 };
