b3c6ef9ea1f6defa9fe2ea57b73efa0ef27e6818
Roberto Rielo <roberto910907@gmail.com>
2023-01-16 00:55:46 -0500

    Added documentation folder

---
+++ docs/app.config.ts
@@ -0,0 +1,28 @@
+export default defineAppConfig({
+  docus: {
+    title: 'Kanban Dashboard',
+    description: 'Create columns, cards and much more!!!',
+    image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+    socials: {
+      twitter: 'RoberRielo',
+      github: 'roberto910907/Kanban-Dashboard',
+    },
+    aside: {
+      level: 0,
+      exclude: []
+    },
+    header: {
+      logo: true,
+      showLinkIcon: true,
+      exclude: []
+    },
+    footer: {
+      iconLinks: [
+        {
+          href: 'https://kanbandash.com',
+          icon: 'IconNuxtLabs'
+        }
+      ]
+    }
+  }
+})
